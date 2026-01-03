dbb1133dbdf81351e70611af220c6d41256196b1
62098 <62098@etu.he2b.be>
2026-01-03 18:52:00 +0100

    fin

--- interview_feedback/interview_feedback/urls.py
+++ interview_feedback/interview_feedback/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # 2. On délègue tout ce qui commence par 'feedback/' à ta nouvelle app
+    # 2. On délègue tout ce qui commence par 'feedback/' à nouvelle app
     path('', include('feedback_portal.urls')),
 ]
